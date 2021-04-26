812e5270d1fdc7ddc142ad8371d34418e0591b89
tdd-test-hogehoge <tdd_test_x74nt3d@sute.jp>
2021-04-26 13:42:59 +0900

    test: 第8章 実装を隠す

--- src/Money.ts
+++ src/Money.ts
@@ -1,7 +1,41 @@
-export default class Money {
+export abstract class Money {
     protected amount!: number;
+    abstract times(multiplier: number): Money;
 
     equals(object: Money) {
         return this.amount === object.amount && this.constructor.name === object.constructor.name;
     }
+
+    static dollar(amount: number): Money {
+        return new Dollar(amount);
+    }
+
+    static franc(amount: number): Money {
+        return new Franc(amount);
+    }
 }
+
+export class Dollar extends Money {
+
+    constructor(amount: number) {
+      super();
+      this.amount = amount;
+    }
+  
+    times(multiplier: number): Money {
+      return new Dollar(this.amount * multiplier);
+    }
+  }
+  
+  export class Franc extends Money {
+
+    constructor(amount: number) {
+      super();
+      this.amount = amount;
+    }
+  
+    times(multiplier: number): Money {
+      return new Franc(this.amount * multiplier);
+    }
+  }
+  
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,22 +1,22 @@
-import Dollar from "../src/Dollar";
-import Franc from "../src/Franc";
+import { Money } from "../src/Money";
 
 describe("moneyTest", () => {
   it("testMultiplication", () => {
-    const five = new Dollar(5);
-    expect(five.times(2)).toEqual(new Dollar(10));
-    expect(five.times(3)).toEqual(new Dollar(15));
+    // const five = new Dollar(5);
+    const five: Money = Money.dollar(5);
+    expect(five.times(2)).toEqual(Money.dollar(10));
+    expect(five.times(3)).toEqual(Money.dollar(15));
   });
   it("testEquality", () => {
-    expect(new Dollar(5).equals(new Dollar(5))).toBe(true);
-    expect(new Dollar(5).equals(new Dollar(6))).toBe(false);
-    expect(new Franc(5).equals(new Franc(5))).toBe(true);
-    expect(new Franc(5).equals(new Franc(6))).toBe(false);
-    expect(new Franc(5).equals(new Dollar(5))).toBe(false);
+    expect(Money.dollar(5).equals(Money.dollar(5))).toBe(true);
+    expect(Money.dollar(5).equals(Money.dollar(6))).toBe(false);
+    expect(Money.franc(5).equals(Money.franc(5))).toBe(true);
+    expect(Money.franc(5).equals(Money.franc(6))).toBe(false);
+    expect(Money.franc(5).equals(Money.dollar(5))).toBe(false);
   });
   it("testFracMultiplication", () => {
-    const five = new Franc(5);
-    expect(five.times(2)).toEqual(new Franc(10));
-    expect(five.times(3)).toEqual(new Franc(15));
+    const five: Money = Money.franc(5);
+    expect(five.times(2)).toEqual(Money.franc(10));
+    expect(five.times(3)).toEqual(Money.franc(15));
   });
 });
